8fe04b72f7aa14e5854deabffef553567598f925
security <security@ellipse1concierge.com>
2024-05-07 07:14:17 -0400

    build chatbot

---
+++ app/api/chat/route.ts
@@ -0,0 +1,23 @@
+import { OpenAIClient, AzureKeyCredential } from '@azure/openai'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+
+// Create an OpenAI API client
+const client = new OpenAIClient(
+  'https://veryopenai.openai.azure.com/',
+  new AzureKeyCredential(process.env.NEXT_PUBLIC_AZURE_OPENAI_API_KEY!)
+)
+
+export async function POST(req: Request) {
+  const { messages } = await req.json()
+
+  // Ask Azure OpenAI for a streaming chat completion given the prompt
+  const response = await client.streamChatCompletions(
+    'VeryOpenAI',
+    messages
+  )
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response)
+  // Respond with the stream
+  return new StreamingTextResponse(stream)
+}
