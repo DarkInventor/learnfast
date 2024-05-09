2d33ec640342ef713dc9c1e3bc136f072ffd46ed
security <security@ellipse1concierge.com>
2024-05-09 03:22:13 -0400

    fix maxDuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,10 @@
 'use client'
 
-// import { useChat } from 'ai/react'
-
-// export default function Home() {
-//   const { messages, input, handleInputChange, handleSubmit } = useChat()
-//   return (
-//     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-//       {messages.map(m => (
-//         <div key={m.id} className="whitespace-pre-wrap">
-//           {m.role === 'user' ? 'User: ' : 'AI: '}
-//           {m.content}
-//         </div>
-//       ))}
-
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-//           value={input}
-//           placeholder="Say something..."
-//           onChange={handleInputChange}
-//         />
-//       </form>
-//     </div>
-//   )
-// }
-
 // Final Version
 import React, { useEffect } from 'react';
 import { useChat } from 'ai/react';
+
+export const maxDuration = 300;
 
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit, stop, isLoading } = useChat();
